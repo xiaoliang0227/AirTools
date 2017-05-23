cacbdc8389ab4905513d4d6e20df332d3ba037da
赵永亮 <zhaoyongliang@zhaoyongliangdeMacBook-Pro.local>
2017-05-23 15:20:28 +0800

    优化 webview加载速度

--- app/src/main/java/com/demo/zyl/airtools/MainActivity.kt
+++ app/src/main/java/com/demo/zyl/airtools/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.demo.zyl.airtools
 
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.view.KeyEvent
@@ -17,35 +23,62 @@ class MainActivity : AppCompatActivity() {
 
     private var needClearHistory: Boolean = false
 
+    private var handler: Handler = Handler()
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                loadData(CommonConsts.URL_AIR_NEWS)
+                loadData(CommonConsts.URL_AIR_NEWS, true)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                loadData(CommonConsts.URL_CTRIP_AIR_TICKET)
+                loadData(CommonConsts.URL_CTRIP_AIR_TICKET, true)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                loadData(CommonConsts.URL_CHIP_AIR_TICKET)
+                loadData(CommonConsts.URL_CHIP_AIR_TICKET, true)
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
-    private fun  loadData(url: String) {
-        needClearHistory = true
-//        content?.clearCache(true)
-        var settings: WebSettings = content!!.settings
-        settings.javaScriptEnabled = true
-        settings.domStorageEnabled = true
-        settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+    private fun  loadData(url: String, needDelay: Boolean) {
+        loadBlankPage()
+        delayLoadPage(url, needDelay)
+    }
 
-        initWebViewClient()
-        initWebViewBackListener()
-        content?.loadUrl(url)
+    private fun  delayLoadPage(url: String, needDelay: Boolean) {
+        val duration:Long = if (needDelay) 100 else 0
+        handler.postDelayed({
+            needClearHistory = true
+            var settings: WebSettings = content!!.settings
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.setRenderPriority(WebSettings.RenderPriority.HIGH)
+            settings.setAppCacheEnabled(true)
+            settings.databaseEnabled = true
+            settings.allowFileAccess = true
+            if (isNetworkAvailable()) {
+                settings.cacheMode = WebSettings.LOAD_DEFAULT
+            } else {
+                settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+            }
+
+            initWebViewClient()
+            initWebViewBackListener()
+            content?.loadUrl(url)
+        }, duration)
+    }
+
+    private fun loadBlankPage() {
+        content?.loadUrl("about:blank")
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        var cm: ConnectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        var info: NetworkInfo = cm.activeNetworkInfo
+        return info != null && info.isAvailable
     }
 
     private fun initWebViewBackListener() {
@@ -66,8 +99,10 @@ class MainActivity : AppCompatActivity() {
         content?.setWebViewClient(object : WebViewClient() {
 
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                content?.loadUrl(url)
-                return false
+                if (!url!!.startsWith("ctrip:")) {
+                    content?.loadUrl(url)
+                }
+                return true
             }
 
             override fun doUpdateVisitedHistory(view: WebView?, url: String?, isReload: Boolean) {
@@ -95,7 +130,7 @@ class MainActivity : AppCompatActivity() {
         navigation = findViewById(R.id.navigation) as BottomNavigationView
         content = findViewById(R.id.content) as WebView
 
-        loadData(CommonConsts.URL_AIR_NEWS)
+        loadData(CommonConsts.URL_AIR_NEWS, false)
     }
 
     private fun initEvent() {
